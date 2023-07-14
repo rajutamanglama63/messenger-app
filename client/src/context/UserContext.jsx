511374ec723a661538e2362bb052ecb25a1bc223
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-07-14 15:12:42 +0545

    feat: scrollable sidebar

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -6,15 +6,67 @@ export const UserContextProvider = ({ children }) => {
   const users = [
     {
       id: 1,
-      username: "Kamini",
+      username: "Padmini",
     },
     {
       id: 2,
-      username: "Damini",
+      username: "Daamenee",
     },
     {
       id: 3,
-      username: "Padmini",
+      username: "Padmawoti",
+    },
+    {
+      id: 3,
+      username: "Padmawoti",
+    },
+    {
+      id: 4,
+      username: "Padmawoti",
+    },
+    {
+      id: 5,
+      username: "Padmawoti",
+    },
+    {
+      id: 6,
+      username: "Padmawoti",
+    },
+    {
+      id: 7,
+      username: "Padmawoti",
+    },
+    {
+      id: 8,
+      username: "Padmawoti",
+    },
+    {
+      id: 9,
+      username: "Padmawoti",
+    },
+    {
+      id: 10,
+      username: "Padmawoti",
+    },
+    {
+      id: 11,
+      username: "Padmawoti",
+    },
+    {
+      id: 12,
+      username: "Padmawoti",
+    },
+    {
+      id: 13,
+      username: "Padmawoti",
+    },
+    {
+      id: 14,
+      username: "Padmawoti",
+    },
+    {
+      id: 15,
+      username: "Padmawoti",
     },
   ];
 
